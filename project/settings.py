a7c549a106904c624bb6d31a4d7cfe71561e3f90
junglewill <b04703008@ntu.edu.tw>
2020-10-20 05:57:30 +0000

    Modified index.html in sightings

--- project/settings.py
+++ project/settings.py
@@ -15,12 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h0$2^w2bqz-udpgac!x0x8)_1f&=ulsn60mv278-@cr1cs(u%!'
+SECRET_KEY = 'ddd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
