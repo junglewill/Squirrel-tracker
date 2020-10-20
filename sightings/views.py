cb1e39c3f71cdfd0c2bc3ff0db12fb3765c63b0f
pl2775 <pl2775@columbia.edu>
2020-10-20 05:57:30 +0000

    Modified index.html in sightings

--- sightings/views.py
+++ sightings/views.py
@@ -5,16 +5,29 @@ from django.shortcuts import get_object_or_404
 
 def index(request):
     squirrels = Squirrel.objects.all()
+
     context = {
             'squirrels': squirrels,
             }
+    field_list = [field.name for field in Squirrel._meta.get_fields()]
     return render(request, "sightings/index.html", context)
 
 def detail(request, Unique_Squirrel_ID):
 
     squirrel = get_object_or_404(Squirrel, pk=Unique_Squirrel_ID)
+    x = getattr(squirrel, 'Latitude')
+    y = getattr(squirrel, 'Longtitude')
+    unique_id = getattr(squirrel, 'Unique_Squirrel_ID')
+    date = getattr(squirrel, 'Date')
+    age = getattr(squirrel, 'Age')
+    pf_color = getattr(squirrel, 'Primary_Fur_Color')
     context = {
-            'squirrel' : squirrel,
+            'squirrel_la': x,
+            'squirrel_lo': y,
+            'unique_sid': unique_id,
+            'date': date,
+            'age': age,
+            'pf_color': pf_color,
             }
     return render(request, 'sightings/detail.index', context)
 
