afaf4168de1c5377613238fec180e68f744ecd8e
junglewill <b04703008@ntu.edu.tw>
2020-10-24 04:00:11 +0000

    Modified view.py

--- sightings/views.py
+++ sightings/views.py
@@ -20,4 +20,40 @@ def squirrel_details(request, Unique_Squirrel_ID):
     }
     return render(request, 'sightings/detail.html', context)
 
+def request_a_pet(request):
+    obj = Squirrel()
+    obj.Latitude = 'This is the title'
+    obj.Longitude = 'This is the content'
+    obj.Unique_Squirrel_ID
+    obj.Shift
+    obj.Date
+    obj.Age
+    obj.Primary_Fur_Color
+obj.Location
+obj.Specific_Location
+obj.Running
+obj.Chasing
+obj.Climbing
+obj.Eating
+obj.Foraging
+obj.Other_Activities
+obj.Kuks
+obj.Quaas
+obj.Moans
+obj.Tail_Flags
+obj.Tail_Twitches
+obj.Approaches
+obj.Indifferent
+obj.Runs_From
+    article.save()
+    if request.method == 'POST':
+        form = AdoptRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({})
+        else:
+            return JsonResponse({'errors': form.errors}, status=400)
+
+    return JsonResponse({})
+
 # Create your views here.
