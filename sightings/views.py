a71f2521e0301c3e84a6de3e52c6f4cad26bdce1
junglewill <b04703008@ntu.edu.tw>
2020-10-26 02:56:08 +0000

    Modify the views

--- sightings/views.py
+++ sightings/views.py
@@ -24,28 +24,29 @@ def request_a_pet(request):
     obj = Squirrel()
     obj.Latitude = 'This is the title'
     obj.Longitude = 'This is the content'
-    obj.Unique_Squirrel_ID
+    obj.Unique_Squirrel_ID = 'This is the squirrel ID'
     obj.Shift
     obj.Date
     obj.Age
     obj.Primary_Fur_Color
-obj.Location
-obj.Specific_Location
-obj.Running
-obj.Chasing
-obj.Climbing
-obj.Eating
-obj.Foraging
-obj.Other_Activities
-obj.Kuks
-obj.Quaas
-obj.Moans
-obj.Tail_Flags
-obj.Tail_Twitches
-obj.Approaches
-obj.Indifferent
-obj.Runs_From
+    obj.Location
+    obj.Specific_Location
+    obj.Running
+    obj.Chasing
+    obj.Climbing
+    obj.Eating
+    obj.Foraging
+    obj.Other_Activities
+    obj.Kuks
+    obj.Quaas
+    obj.Moans
+    obj.Tail_Flags
+    obj.Tail_Twitches
+    obj.Approaches
+    obj.Indifferent
+    obj.Runs_From
     article.save()
+
     if request.method == 'POST':
         form = AdoptRequestForm(request.POST)
         if form.is_valid():
