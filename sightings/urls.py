b977a499e533bd56d59250cda1953cdf72c2c683
junglewill <b04703008@ntu.edu.tw>
2020-10-27 02:29:39 +0000

    Update urls of the deatiled information

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'sightings'
 urlpatterns = [
         path('', views.index, name='index'),
-        re_path(r'^sightings/(?P<Unique_Squirrel_ID>[0-9A-Z -]+)/$', views.squirrel_details, name='details'),
+        re_path(r'^(?P<Unique_Squirrel_ID>[0-9A-Z -]+)/$', views.squirrel_details, name='details'),
 ]
