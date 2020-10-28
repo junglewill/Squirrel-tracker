50b3e0106dd6567ea03d2630430adae1275e062d
junglewill <b04703008@ntu.edu.tw>
2020-10-28 01:32:38 +0000

    Edit regex urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'sightings'
 urlpatterns = [
         path('', views.index, name='index'),
-        re_path(r'^(?P<Unique_Squirrel_ID>[0-9A-Z -]+)/$', views.squirrel_details, name='details'),
+        re_path(r'^(?P<Unique_Squirrel_ID>[0-9A-Za-z -]+)/$', views.squirrel_details, name='details'),
         path('add/', views.add, name='add'),
         path('request/', views.add_squirrel, name='request'),
         ]
